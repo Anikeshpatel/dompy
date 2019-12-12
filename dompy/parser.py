60f7e161fb11ee260d3456385f22f5e438bdb743
Anikesh Patel <developeranikesh@gmail.com>
2019-12-12 15:19:15 +0530

    adding tag parsing support

--- dompy/parser.py
+++ dompy/parser.py
@@ -6,16 +6,40 @@ class DompyParser:
         position = 0
         for token in input:
             position += 1
-            if token == '<' and input[position + 1] != '!':
+            if token == '<' and input[position + 1] != '!' and input[position] != '/':
                 print(input[position: input.find('>', position)])
 
     @staticmethod
-    def __resolveTag(tagToken: str) -> Element:
+    def __resolveTag(cls, tagToken: str) -> Element:
         children = []
-        if (hasChildren(tagToken)):
-            for child in parseChildren(tagToken):
+        if (cls.__hasChildren(tagToken)):
+            for child in cls.__parseChildren(tagToken):
                 children.append(DompyParser.__resolveTag(child))
-        element = parseTag(tagToken)
+        element = cls.__parseTag(tagToken)
         element.children = children
         return element
 
+    @staticmethod
+    def __hasChildren(token):
+        pass
+
+    @staticmethod
+    def __parseChildren(token):
+        pass
+
+    @staticmethod
+    def __parseTag(token) -> Element:
+        element = Element()
+        token = '<input type="checkbox" class="navigation__checkbox" id="navi-toggle">'
+        for t in token.split(' '):
+            if t.find('<') > -1:
+                element.tag = t[t.find('<') +1 : len(t)]
+            elif t.find('='):
+                key = t.split('=')[0]
+                value = t.split('=')[1].replace('"', '').replace('>', '')
+                if key == 'id':
+                    element.id = value
+                elif key == 'class':
+                    element.classList.append(value)
+                else:
+                    element.attributes[key] = value
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from dompy import Document
 
 if __name__ == "__main__":
-    file = open('index.html', 'r')
-    document = Document.fromFile(file)
-    document.title
-    document.getElementById('header')
-    file.close()
+    # file = open('index.html', 'r')
+    # document = Document.fromFile(file)
+    # document.title
+    # document.getElementById('header')
+    # file.close()
+
+    token = '<input type="checkbox" class="navigation__checkbox" id="navi-toggle">'
+    for t in token.split(' '):
+        if t.find('<') > -1:
+            tag = t[t.find('<') +1 : len(t)]
+            print(tag)
+        elif t.find('='):
+            key = t.split('=')[0]
+            value = t.split('=')[1].replace('"', '').replace('>', '')
+            print({key: value})
+        
